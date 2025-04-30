ae8936f7c46616471af2649a02467d30149af4ff
isaac <isaacbejaranopro@gmail.com>
2025-04-30 19:01:09 +0200

    navigation

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -7,14 +7,34 @@
 // import SomeModule from 'some-node-module';
 
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap';
 // TODO: import individuals
 
 /**
  * Write any other JavaScript below
  */
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
+////
+// Data Injection
+////
+
+const fragment = route => route.replace(".html","").split("/").pop();
+
+// <title>
+////
+((title, fragment) => {
+  const headTitle = document.getElementById('js-head-title');
+  const route = window.location.pathname;
+
+  headTitle.textContent = title + (fragment(route) === "index" ? "" : ` | ${fragment(route)}`);
+})("Jornades Economia Circular 101", fragment);
+
+// .nav-link
+////
+((fragment) => {
+  const navLinks = document.getElementsByClassName('nav-link');
+
+  for (const navLink of navLinks) {
+    const route = fragment(navLink.href) === "index" ? "inici" : fragment(navLink.href);
+    navLink.textContent = route;
+  }
+})(fragment);
