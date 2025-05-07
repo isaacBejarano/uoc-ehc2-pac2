9e4518caaf8d05f5f7cb63945cb69f64b3a19720
isaac <isaacbejaranopro@gmail.com>
2025-05-08 01:11:33 +0200

    index wip

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -8,6 +8,16 @@
 
 // Import all of Bootstrap's JS
 // TODO: import individuals
+import '../../../node_modules/bootstrap/js/dist/modal';
+import '../../../node_modules/bootstrap/js/dist/collapse'; // accordion
+// import '../../../node_modules/bootstrap/js/dist/alert';
+// import '../../../node_modules/bootstrap/js/dist/carousel';
+// import '../../../node_modules/bootstrap/js/dist/dropdown';
+// import '../../../node_modules/bootstrap/js/dist/popover';
+// import '../../../node_modules/bootstrap/js/dist/tab';
+// import '../../../node_modules/bootstrap/js/dist/toast';
+// import '../../../node_modules/bootstrap/js/dist/tooltip';
+// import '../../../node_modules/bootstrap/js/dist/button'; // togglers
 
 /**
  * Write any other JavaScript below
@@ -21,20 +31,22 @@ const fragment = route => route.replace(".html","").split("/").pop();
 
 // <title>
 ////
+
 ((title, fragment) => {
   const headTitle = document.getElementById('js-head-title');
   const route = window.location.pathname;
 
   headTitle.textContent = title + (fragment(route) === "index" ? "" : ` | ${fragment(route)}`);
-})("Jornades Economia Circular 101", fragment);
+})("Circular Economy 101", fragment);
 
 // .nav-link
 ////
+
 ((fragment) => {
   const navLinks = document.getElementsByClassName('nav-link');
 
   for (const navLink of navLinks) {
-    const route = fragment(navLink.href) === "index" ? "inici" : fragment(navLink.href);
+    const route = fragment(navLink.href) === "index" ? "home" : fragment(navLink.href);
     navLink.textContent = route;
   }
 })(fragment);
