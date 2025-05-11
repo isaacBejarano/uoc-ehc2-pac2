6d5fe1c18493180b3df452f3d0479365410f7169
isaac <isaacbejaranopro@gmail.com>
2025-05-12 00:39:48 +0200

    article

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -9,15 +9,6 @@
 // Import all of Bootstrap's JS
 import '../../../node_modules/bootstrap/js/dist/modal';
 import '../../../node_modules/bootstrap/js/dist/collapse'; // accordion
-// FIXME: clean unused
-// import '../../../node_modules/bootstrap/js/dist/alert';
-// import '../../../node_modules/bootstrap/js/dist/carousel';
-// import '../../../node_modules/bootstrap/js/dist/dropdown';
-// import '../../../node_modules/bootstrap/js/dist/popover';
-// import '../../../node_modules/bootstrap/js/dist/tab';
-// import '../../../node_modules/bootstrap/js/dist/toast';
-// import '../../../node_modules/bootstrap/js/dist/tooltip';
-// import '../../../node_modules/bootstrap/js/dist/button'; // togglers
 
 /**
  * Write any other JavaScript below
