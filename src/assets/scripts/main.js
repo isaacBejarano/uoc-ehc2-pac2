7ddacc57797478b61aafd1d274fbe2695b6b4b7d
isaac <isaacbejaranopro@gmail.com>
2025-05-11 06:11:32 +0200

    video gallery

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -9,6 +9,7 @@
 // Import all of Bootstrap's JS
 import '../../../node_modules/bootstrap/js/dist/modal';
 import '../../../node_modules/bootstrap/js/dist/collapse'; // accordion
+// FIXME: clean unused
 // import '../../../node_modules/bootstrap/js/dist/alert';
 // import '../../../node_modules/bootstrap/js/dist/carousel';
 // import '../../../node_modules/bootstrap/js/dist/dropdown';
@@ -27,15 +28,19 @@ import '../../../node_modules/bootstrap/js/dist/collapse'; // accordion
 ////
 
 const fragment = route => route.replace(".html","").split("/").pop();
+const dedasher = str => str.split('-').filter(ch => ch.length).join(" ");
+const capitalize = str => str.split(" ").map(word => word.slice(0,1).toUpperCase() + word.slice(1).toLowerCase()).join(" ");
 
 // <title>
 ////
 
 ((title, fragment) => {
   const headTitle = document.getElementById('js-head-title');
-  const route = window.location.pathname;
-
-  headTitle.textContent = title + (fragment(route) === "index" ? "" : ` | ${fragment(route)}`);
+  const route = window.location.pathname;  
+  const routeFragment = fragment(route);
+  const dedashedFragment = dedasher(routeFragment);
+  const capitalizedFragment = capitalize(dedashedFragment);
+  headTitle.textContent = title + (routeFragment === "index" ? "" : ` | ${capitalizedFragment}`);
 })("Circular Economy 101", fragment);
 
 // .nav-link
@@ -45,7 +50,7 @@ const fragment = route => route.replace(".html","").split("/").pop();
   const navLinks = document.getElementsByClassName('nav-link');
 
   for (const navLink of navLinks) {
-    const route = fragment(navLink.href) === "index" ? "home" : fragment(navLink.href);
+    const route = fragment(navLink.href) === "index" ? "home" : dedasher(fragment(navLink.href));
     navLink.textContent = route;
   }
 })(fragment);
